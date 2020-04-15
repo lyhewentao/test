74725cc7c891c275a6bc688441642f5d276b158d
lyhewentao <39685498+lyhewentao@users.noreply.github.com>
2020-04-15 20:35:27 +0800

    创建文件
    第一次使用github创建文件

---
+++ HelloController.java
@@ -0,0 +1,15 @@
+package com.hewentao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "index";
+    }
+
+
+}
